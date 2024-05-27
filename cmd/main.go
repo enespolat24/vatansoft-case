3cc92f3458618d636aebb314cbf1148aa78cee0e
enespolat24 <enespolat24@gmail.com>
2024-05-27 23:59:31 +0300

    use godotenv

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"vatansoft-case/internal/database"
+	routes "vatansoft-case/internal/route"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	e := echo.New()
+
+	dbConn := database.NewDbConnection()
+	defer dbConn.CloseDbConnection()
+
+	if err := dbConn.AutoMigrate(); err != nil {
+		e.Logger.Fatalf("Failed to migrate database: %v", err)
+	}
+
+	routes.InitRoutes(e)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
